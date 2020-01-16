6ace3401e7bd23cbd56744f473d48c3dc29cc0ad
leylarstamova <59923077+leylarstamova@users.noreply.github.com>
2020-01-17 00:00:35 +0300

    hello name

---
+++ helloname/src/helloname/Main.java
@@ -0,0 +1,13 @@
+package helloname;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Как тебя зовут?");
+        String name = scanner.nextLine();
+        System.out.println("Привет, " + name + "!");
+    }
+}
